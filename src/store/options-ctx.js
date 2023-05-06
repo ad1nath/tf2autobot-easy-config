b7c34f7fc848781177dca4ce9a0784d33192aba4
adinath <adinath@gmail.com>
2023-05-06 18:41:06 +0530

    fix

--- src/store/options-ctx.js
+++ src/store/options-ctx.js
@@ -1,57 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    options: [],
-    editedOptions: [],
-    activeItem: 'miscSettings',
-    currentPath: '',
-    currentDescription: '',
-    descriptions: [],
-    toggleDescription: false,
-    currentValues: {}
-}
+	options: [],
+	editedOptions: [],
+	activeItem: "miscSettings",
+	currentPath: "",
+	currentDescription: "",
+	descriptions: [],
+	toggleDescription: false,
+	currentValues: {},
+};
 
 const set = (obj, str, val) => {
-    str = str.split("_");
-    while (str.length > 1)
-        obj = obj[str.shift()];
-    return obj[str.shift()] = val;
-}
-
+	str = str.split("_");
+	while (str.length > 1) obj = obj[str.shift()];
+	return (obj[str.shift()] = val);
+};
 
 const optionSlice = createSlice({
-    name: 'options',
-    initialState,
-    reducers: {
-        editOption(state, action) {
-            const { optionValue, optionKeys } = action.payload
-            set(state.editedOptions, optionKeys, optionValue)
-            state.currentValues[optionKeys] = optionValue
-        },
-        makeActive(state, action) {
-            state.activeItem = action.payload
-        },
-        setOptions(state, action) {
-            state.options = action.payload
-            state.editedOptions = action.payload
-        },
-        setDescriptions(state, action) {
-            state.descriptions = action.payload
-        },
-        toggleDescriptionBox(state) {
-            state.toggleDescription = !state.toggleDescription
-        },
+	name: "options",
+	initialState,
+	reducers: {
+		editOption(state, action) {
+			const { optionValue, optionKeys } = action.payload;
+			set(state.editedOptions, optionKeys, optionValue);
+			state.currentValues[optionKeys] = optionValue;
+		},
+		makeActive(state, action) {
+			state.activeItem = action.payload;
+		},
+		setOptions(state, action) {
+			state.options = action.payload;
+			state.editedOptions = action.payload;
+		},
+		setDescriptions(state, action) {
+			state.descriptions = action.payload;
+		},
+		toggleDescriptionBox(state) {
+			state.toggleDescription = !state.toggleDescription;
+		},
 
-        setCurrentDescription(state, action) {
-            const { id, description } = action.payload
-            state.currentDescription = description || null
-            state.currentPath = id
-        },
-    },
-})
+		setCurrentDescription(state, action) {
+			const { id, description } = action.payload;
+			state.currentDescription = description || null;
+			state.currentPath = id;
+		},
+	},
+});
 
-export const optionActions = optionSlice.actions
-export default optionSlice.reducer
-
-
-
+export const optionActions = optionSlice.actions;
+export default optionSlice.reducer;
