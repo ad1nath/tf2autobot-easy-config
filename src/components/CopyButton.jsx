dff13a669605aa42a3e3209fcbf9155665632bc2
adinath <adinath@gmail.com>
2023-05-06 18:40:48 +0530

    show copied message a little longer

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -1,21 +1,35 @@
-import { ClipboardIcon } from "@heroicons/react/24/outline";
+import {
+	ClipboardDocumentCheckIcon,
+	ClipboardDocumentIcon,
+} from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const CopyButton = ({ onClick, classes }) => {
-  return (
-    <button
-      onClick={onClick}
-      className={`${classes} group relative invisible content-bg-white group-hover/option:visible`}
-    >
-      <span
-        data-copy="copy command"
-        data-copied="copied!"
-        className="group-hover:scale-100 inline transition-transform duration-300 origin-left left-full  mr-2  -translate-y-1/4 scale-0 bg-gray-600 px-1 text-sm text-gray-100 rounded-md absolute 
-    before:content-[attr(data-copy)] group-active:before:content-[attr(data-copied)]
+	const [copy, setCopy] = useState("Copy Command");
+	return (
+		<button
+			onClick={() => {
+				setCopy("Copied Command");
+				setTimeout(() => {
+					setCopy("Copy Command");
+				}, 1500);
+				onClick();
+			}}
+			className={`${classes} group relative invisible content-bg-white group-hover/option:visible`}
+		>
+			<span
+				data-copy={copy}
+				className="group-hover:scale-100 inline transition-transform duration-300 origin-left left-full  mr-2  -translate-y-1/4 scale-0 bg-gray-600 px-1 text-sm text-gray-100 rounded-md absolute 
+    before:content-[attr(data-copy)]
     "
-      ></span>
-      <ClipboardIcon className="h-5 w-5 self-center text-white active:text-lime-500" />{" "}
-    </button>
-  );
+			/>
+			{copy === "Copy Command" ? (
+				<ClipboardDocumentIcon className="h-5 w-5 self-center text-white active:text-lime-500" />
+			) : (
+				<ClipboardDocumentCheckIcon className="h-5 w-5 self-center text-lime-500" />
+			)}
+		</button>
+	);
 };
 
 export default CopyButton;
