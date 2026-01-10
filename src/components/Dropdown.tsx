5bcb5b322ef85981034f9e1f00d8f5fe553951c6
ad1nath <adinathuparkar@gmail.com>
2026-01-10 22:59:27 +0530

    update dropdown style

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { Select } from "@base-ui/react/select";
-import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpDownIcon,
+} from "@heroicons/react/20/solid";
 
 export default function Dropdown({ options, defaultValue, onValueChange }) {
   const items = options.map((item) => {
@@ -15,25 +19,74 @@ export default function Dropdown({ options, defaultValue, onValueChange }) {
       defaultValue={defaultValue}
       items={items}
     >
-      <Select.Trigger className="flex h-10 min-w-36 items-center justify-between gap-3 rounded-md border border-gray-200 pr-3 pl-3.5 text-base bg-[canvas] text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 data-[popup-open]:bg-gray-100">
+      <Select.Trigger
+        className="
+          group inline-flex h-9 min-w-36 items-center justify-between gap-2
+          rounded-md bg-slate-700 px-3 text-sm text-slate-200
+          border border-slate-900
+          hover:bg-slate-800 active:bg-slate-900
+          focus-visible:outline focus-visible:outline-2 focus-visible:outline-lime-500
+          data-[popup-open]:bg-slate-900
+        "
+      >
         <Select.Value />
-        <Select.Icon className="flex">
-          <ChevronUpDownIcon />
+        <Select.Icon
+          className="
+            transition-transform duration-300
+            group-data-[popup-open]:rotate-180
+            text-slate-300 group-hover:text-lime-500
+          "
+        >
+          <ChevronDownIcon className="h-5 w-5" />
         </Select.Icon>
       </Select.Trigger>
       <Select.Portal>
-        <Select.Positioner
-          className="outline-none select-none z-10"
-          sideOffset={8}
-        >
-          <Select.Popup className="group min-w-[var(--anchor-width)] origin-[var(--transform-origin)] bg-clip-padding rounded-md bg-[canvas] text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[side=none]:min-w-[calc(var(--anchor-width)+1rem)] data-[side=none]:data-[ending-style]:transition-none data-[starting-style]:scale-90 data-[starting-style]:opacity-0 data-[side=none]:data-[starting-style]:scale-100 data-[side=none]:data-[starting-style]:opacity-100 data-[side=none]:data-[starting-style]:transition-none dark:shadow-none dark:outline-gray-300">
+        <Select.Positioner className="z-10" sideOffset={8}>
+          <Select.Popup
+            className="
+              min-w-[var(--anchor-width)]
+              rounded-lg border border-slate-900
+              bg-slate-700 shadow-lg
+              overflow-hidden
+              outline-none
+              data-[starting-style]:opacity-0 data-[ending-style]:opacity-0
+              data-[starting-style]:scale-95 data-[ending-style]:scale-95
+              transition-[opacity,transform]
+            "
+          >
             <Select.ScrollUpArrow className="top-0 z-[1] flex h-4 w-full cursor-default items-center justify-center rounded-md bg-[canvas] text-center text-xs before:absolute data-[side=none]:before:top-[-100%] before:left-0 before:h-full before:w-full before:content-['']" />
             <Select.List className="relative py-1 scroll-py-6 overflow-y-auto max-h-[var(--available-height)]">
               {items.map(({ label, value }) => (
                 <Select.Item
                   key={label}
                   value={value}
-                  className="grid cursor-default grid-cols-[0.75rem_1fr] items-center gap-2 py-2 pr-4 pl-2.5 text-sm leading-4 outline-none select-none group-data-[side=none]:pr-12 group-data-[side=none]:text-base group-data-[side=none]:leading-4 data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900 pointer-coarse:py-2.5 pointer-coarse:text-[0.925rem]"
+                  className="
+                  grid grid-cols-[0.75rem_1fr] items-center gap-2
+                  py-2 pr-4 pl-2.5
+                  text-sm leading-4
+                  select-none outline-none
+                  cursor-pointer
+                text-slate-200
+                  /* highlighted state */
+                  data-[highlighted]:relative
+                  data-[highlighted]:z-0
+                  data-[highlighted]:text-lime-400
+                  data-[highlighted]:before:absolute
+                  data-[highlighted]:before:inset-x-1
+                  data-[highlighted]:before:inset-y-0
+                  data-[highlighted]:before:-z-10
+                  data-[highlighted]:before:rounded-sm
+                  data-[highlighted]:before:bg-gray-900
+                  data-[selected]:text-lime-400
+                  /* group variants */
+                  group-data-[side=none]:pr-12
+                  group-data-[side=none]:text-base
+                  group-data-[side=none]:leading-4
+
+                  /* coarse pointer devices */
+                  pointer-coarse:py-2.5
+                  pointer-coarse:text-[0.925rem]
+                "
                 >
                   <Select.ItemIndicator className="col-start-1">
                     <CheckIcon className="size-3" />
