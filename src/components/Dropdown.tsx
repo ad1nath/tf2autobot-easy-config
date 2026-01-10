7f5660889ffa25a3d7aa1835df9731896c1f3f0b
ad1nath <adinathuparkar@gmail.com>
2026-01-10 19:59:19 +0530

    add baseUI select

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,58 +1,53 @@
-import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import * as React from "react";
+import { Select } from "@base-ui/react/select";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-const Dropdown = ({ options, defaultValue, sendSelected }) => {
-  const [show, setShow] = useState(false);
-  const [selected, setSelected] = useState(
-    options.find((option) => {
-      return option.value === defaultValue;
-    }).name
-  );
-
-  const handleSelect = (item) => {
-    setSelected(item.name);
-    setShow((pre) => !pre);
-    sendSelected(item.value);
-  };
-
+export default function Dropdown({ options, defaultValue, onValueChange }) {
+  const items = options.map((item) => {
+    return {
+      label: item.name,
+      value: item.value,
+    };
+  });
   return (
-    <div>
-      <button
-        onClick={() => {
-          setShow((pre) => !pre);
-        }}
-        className={`inline-flex group p-1 px-2 bg-slate-700 align-center hover:bg-slate-900 active:bg-slate-900  border-slate-900 border-1 rounded-md text-sm text-center text-slate-200`}
-      >
-        {selected}
-        <ChevronDownIcon
-          className={`w-5 h-5 ml-2 group-hover:text-lime-500 transition-transform  duration-300 text-black ${
-            show ? "rotate-180" : ""
-          }`}
-        />
-      </button>
-      <ul
-        className={`absolute border-slate-900 border-2 ${
-          show ? "visible" : "invisible"
-        }  py-2 bg-slate-700 mt-2 shadow-md  max-h-52 h-max overflow-y-auto
-          scrollbar-thin scrollbar-thumb-slate-500 scrollbar-track-transparent border scrollbar-thumb-rounded-lg
-          rounded-lg`}
-      >
-        {options.map((item) => (
-          <li
-            className={`text-slate-lime px-2 pt-0.5 hover:bg-slate-800 hover:cursor-pointer hover:text-lime-500 ${
-              item.name === selected ? "text-lime-600 " : ""
-            }`}
-            key={item.name}
-            name={item.name}
-            value={item.value}
-            onMouseDown={handleSelect.bind(this, item)}
-          >
-            {item.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Select.Root
+      onValueChange={onValueChange}
+      defaultValue={defaultValue}
+      items={items}
+    >
+      <Select.Trigger className="flex h-10 min-w-36 items-center justify-between gap-3 rounded-md border border-gray-200 pr-3 pl-3.5 text-base bg-[canvas] text-gray-900 select-none hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 data-[popup-open]:bg-gray-100">
+        <Select.Value />
+        <Select.Icon className="flex">
+          <ChevronUpDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Positioner
+          className="outline-none select-none z-10"
+          sideOffset={8}
+        >
+          <Select.Popup className="group min-w-[var(--anchor-width)] origin-[var(--transform-origin)] bg-clip-padding rounded-md bg-[canvas] text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[side=none]:min-w-[calc(var(--anchor-width)+1rem)] data-[side=none]:data-[ending-style]:transition-none data-[starting-style]:scale-90 data-[starting-style]:opacity-0 data-[side=none]:data-[starting-style]:scale-100 data-[side=none]:data-[starting-style]:opacity-100 data-[side=none]:data-[starting-style]:transition-none dark:shadow-none dark:outline-gray-300">
+            <Select.ScrollUpArrow className="top-0 z-[1] flex h-4 w-full cursor-default items-center justify-center rounded-md bg-[canvas] text-center text-xs before:absolute data-[side=none]:before:top-[-100%] before:left-0 before:h-full before:w-full before:content-['']" />
+            <Select.List className="relative py-1 scroll-py-6 overflow-y-auto max-h-[var(--available-height)]">
+              {items.map(({ label, value }) => (
+                <Select.Item
+                  key={label}
+                  value={value}
+                  className="grid cursor-default grid-cols-[0.75rem_1fr] items-center gap-2 py-2 pr-4 pl-2.5 text-sm leading-4 outline-none select-none group-data-[side=none]:pr-12 group-data-[side=none]:text-base group-data-[side=none]:leading-4 data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900 pointer-coarse:py-2.5 pointer-coarse:text-[0.925rem]"
+                >
+                  <Select.ItemIndicator className="col-start-1">
+                    <CheckIcon className="size-3" />
+                  </Select.ItemIndicator>
+                  <Select.ItemText className="col-start-2">
+                    {label}
+                  </Select.ItemText>
+                </Select.Item>
+              ))}
+            </Select.List>
+            <Select.ScrollDownArrow className="bottom-0 z-[1] flex h-4 w-full cursor-default items-center justify-center rounded-md bg-[canvas] text-center text-xs before:absolute before:left-0 before:h-full before:w-full before:content-[''] bottom-0 data-[side=none]:before:bottom-[-100%]" />
+          </Select.Popup>
+        </Select.Positioner>
+      </Select.Portal>
+    </Select.Root>
   );
-};
-
-export default Dropdown;
+}
